79a509fc3be50ca3ff68fca86c74a0ea3170e92d
Daniel Marques <danielbmarques@gmail.com>
2026-03-14 20:53:34 +0000

    Update paperboat to v0.1.1

--- Formula/paperboat.rb
+++ Formula/paperboat.rb
@@ -2,23 +2,23 @@
 # frozen_string_literal: true
 
 class Paperboat < Formula
-  desc "AI Agent Orchestrator"
+  desc "AI agents can be used to accomplish nearly anything"
   homepage "https://github.com/dbmrq/paperboat"
-  version "0.2.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dbmrq/paperboat/releases/download/v0.2.0/paperboat_0.2.0_Darwin_x86_64.tar.gz"
-      sha256 "dd7267b693b7f27a2efba30d0df8d14b7134a646ae6be9fa57abdaab01ecbee7"
+      url "https://github.com/dbmrq/paperboat/releases/download/v0.1.1/paperboat_0.1.1_Darwin_x86_64.tar.gz"
+      sha256 "81fd5f4379a47474b7119a9d17d608e12ebf3020884be22f9901528c27cc3a0a"
 
       def install
         bin.install "paperboat"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dbmrq/paperboat/releases/download/v0.2.0/paperboat_0.2.0_Darwin_arm64.tar.gz"
-      sha256 "61a0df2edea9d691916255485455b698aaa91aff71182bb38bd914d4e84cb20b"
+      url "https://github.com/dbmrq/paperboat/releases/download/v0.1.1/paperboat_0.1.1_Darwin_arm64.tar.gz"
+      sha256 "1d4e882462dd9d6a5811b63178bbfa6c0f36012d2dee621c09c71b5058189398"
 
       def install
         bin.install "paperboat"
@@ -28,15 +28,15 @@ class Paperboat < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dbmrq/paperboat/releases/download/v0.2.0/paperboat_0.2.0_Linux_x86_64.tar.gz"
-      sha256 "b7f5e5e56637e98abcbf06b930a0acdd2dc0819239400b924aad24fa9f996d0f"
+      url "https://github.com/dbmrq/paperboat/releases/download/v0.1.1/paperboat_0.1.1_Linux_x86_64.tar.gz"
+      sha256 "17b0d49d6adf9eee6fa9ff9ed8d441d44c07c5b23f736e89b11e7a83b89af29d"
       def install
         bin.install "paperboat"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dbmrq/paperboat/releases/download/v0.2.0/paperboat_0.2.0_Linux_arm64.tar.gz"
-      sha256 "b4966f0e118c1443084204780e071478fab70fa319c5322ee954e98f63ad2edf"
+      url "https://github.com/dbmrq/paperboat/releases/download/v0.1.1/paperboat_0.1.1_Linux_arm64.tar.gz"
+      sha256 "ba13e646a884995203a7dad9a984defad2a5c91511ffcefb4da374097f72e13e"
       def install
         bin.install "paperboat"
       end
